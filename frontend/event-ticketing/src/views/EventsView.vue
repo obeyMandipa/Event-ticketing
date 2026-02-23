8469ffcbd097e89a819663b36d5f1b52316c32c6
Rufaro Mandipa <mandipaobey@gmail.com>
2026-02-23 15:15:09 +0200

    created the events view page

--- frontend/event-ticketing/src/views/EventsView.vue
+++ frontend/event-ticketing/src/views/EventsView.vue
@@ -1,12 +1,45 @@
 <template>
-  <div class="events-view">
-    <h1>Events View</h1>
+  <div >
+    <h1>Browse Events</h1>
+    <nav><router-link to="/admin">Admin Dashboard</router-link></nav>
+    <div v-for="event in events" :key="event._id" class="event">
+        <h2>{{ event.name }}</h2>
+        <p>{{ event.description }}</p>
+        <p>Date: {{ event.date }}</p>
+        <p>Venue: {{ event.venue }}</p>
+        <p>${{ event.price }}/seat ({{ event.seats.filter(s => s.available).length }} available)</p>
+
+        <router-link :to="`/purchase/${event._id}`" class="buy-btn">Buy tickets</router-link>
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+    data () {
+        return {
+            events: [],
+        }
+    },
+
+    async mounted () {
+        const {data} = await this.$axios.get('/events');
+        this.events = data;
+    },
+}
 
 </script>
 
 <style>
+.event { 
+    border: 1px solid #ddd;
+     padding: 20px;
+      margin: 20px 0; 
+    }
+.buy-btn {
+    background: #007bff;
+    color: white; 
+    padding: 10px 20px; 
+    text-decoration: none; 
+}
 </style>
